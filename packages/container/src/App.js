c310bbba90e1752be33a708497cdf14165a153dd
Marian Krekhovetskyi <m.krekhovetskyi@astoundcommerce.com>
2025-01-09 22:31:49 +0200

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MarketingApp from './components/MarketingApp';
+
+export default function App() {
+    return (
+        <div>
+            <h1>Hi there!</h1>
+            <hr />
+            <MarketingApp />
+        </div>
+    );
+}
