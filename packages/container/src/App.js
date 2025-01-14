dcfe9ea65b1c5ac8535f84f7c4ad228ee7a85042
Marian Krekhovetskyi <m.krekhovetskyi@astoundcommerce.com>
2025-01-14 21:09:19 +0200

    Fix bugs in container

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import MarketingApp from './components/MarketingApp';
 
-export default function App() {
-    return (
-        <div>
-            <h1>Hi there!!</h1>
-            <hr/>
-            <MarketingApp />
-        </div>
-    );
-}
+export default () => {
+  return (
+    <div>
+      <h1>Hi there!!!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
+};
