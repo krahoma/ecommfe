e7500144cf2f114a5324cd559dcd086f482fbce3
Marian Krekhovetskyi <m.krekhovetskyi@astoundcommerce.com>
2025-01-14 21:05:03 +0200

    Fix bugs in marketing

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,20 +1,21 @@
-import React from "react";
-import { StylesProvider } from "@material-ui/core/styles";
-import { Switch, BrowserRouter, Route } from "react-router-dom";
-import Landing from "./components/Landing";
-import Pricing from "./components/Pricing";
+import React from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
 
-const App = () => {
-    return <div>
-        <StylesProvider>
-            <BrowserRouter>
-                <Switch>
-                    <Route exact path="/pricing" component={Pricing} />
-                    <Route path="/" component={Landing} />
-                </Switch>
-            </BrowserRouter>
-        </StylesProvider>
-    </div>;
-}
+import Landing from './components/Landing';
+import Pricing from './components/Pricing';
 
-export default App;
+export default () => {
+  return (
+    <div>
+      <StylesProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/pricing" component={Pricing} />
+            <Route path="/" component={Landing} />
+          </Switch>
+        </BrowserRouter>
+      </StylesProvider>
+    </div>
+  );
+};
