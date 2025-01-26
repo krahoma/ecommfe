875d4881b9c081fd9f0f0e491512d7a7eefdd56b
Marian Krekhovetskyi <m.krekhovetskyi@astoundcommerce.com>
2025-01-26 13:58:52 +0200

    Add some fixes for marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
-};
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  var history = defaultHistory || createMemoryHistory({
+    initialEntries: [initialPath],
+  });
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+}
 
 // If we are in development and in isolation,
 // call mount immediately
@@ -13,7 +30,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
